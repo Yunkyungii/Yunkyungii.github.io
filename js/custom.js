165b99b419c915c80f58d40272a7fa0a87ab6d97
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-08-31 18:19:37 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,24 @@
 const panels = gsap.utils.toArray(".MainContent .itm");
 
 
-const H = gsap.to(panels, {
-    xPercent: -100 * (panels.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".MainContent",
-        pin: true,
-        start: "top top",
+// const H = gsap.to(panels, {
+//     xPercent: -100 * (panels.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".MainContent",
+//         pin: true,
+//         start: "top top",
 
-        scrub: 0, // 숫자에따라 변함이 있음
-        markers: true,
-        snap: {
-            snapTo: 1 / (panels.length - 1),
-            inertia: false,
-            duration: { min: 0.1, max: 0.1 }
-        },
-        //end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
-    }
-});
+//         scrub: 0, // 숫자에따라 변함이 있음
+//         markers: true,
+//         snap: {
+//             snapTo: 1 / (panels.length - 1),
+//             inertia: false,
+//             duration: { min: 0.1, max: 0.1 }
+//         },
+//         //end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
+//     }
+// });
 
 const BOX =
     gsap.from('.MainIntro .box', {
@@ -64,33 +64,135 @@ BOX.play(3);
 
 
 
-const D = gsap.to('.MainDal .dal', {
-    x: 400,
-    y: 1000,
-    rotation: 720,
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".MainDal",
-        pin: true,
-        start: "top top",
-        end: "+=1600",
-        scrub: 0, // 숫자에따라 변함이 있음
-        //markers: true
-        // snap: {
-        //     snapTo: 1 / (panels.length - 1),
-        //     inertia: false,
-        //     duration: { min: 0.1, max: 0.1 }
-        // },
-        //end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
-    }
+// const D = gsap.to('.MainDal .dal', {
+//     x: 400,
+//     y: 1000,
+//     rotation: 720,
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".MainDal",
+//         pin: true,
+//         start: "top top",
+//         end: "+=1600",
+//         scrub: 0, // 숫자에따라 변함이 있음
+//         //markers: true
+//         // snap: {
+//         //     snapTo: 1 / (panels.length - 1),
+//         //     inertia: false,
+//         //     duration: { min: 0.1, max: 0.1 }
+//         // },
+//         //end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
+//     }
 
+// });
+const S =
+    gsap.from('.project h2', {
+        y: -300,
+        duration: 2,
+        start: "top center",
+    });
+
+gsap.from('.project .left', {
+    x: -300,
+    duration: 2,
+    start: "top center",
 });
 
-const MENU_BTN = document.querySelector('.MainIntro .menu');
+gsap.from('.project .right', {
+    x: 300,
+    duration: 2,
+    start: "top center",
+});
+
+
+gsap.to(S, {
+    scrollTrigger: {
+        trigger: ".MainProject",
+        start: "top center",
+    }
+})
+
+
+const MENU_BTN = document.querySelector('.btn');
 const MENU = document.querySelector('.MainIntro .menu_con');
+const CON = document.querySelector('.inner')
+//const BTN = document.querySelector('.btn');
 
 
 MENU_BTN.addEventListener('click', e => {
     e.preventDefault();
     MENU.classList.toggle('on');
+    CON.classList.toggle('on');
+
+    const LINK = document.querySelectorAll('.menu_con a');
+
+
+    LINK.forEach((it, idx) => {
+        it.addEventListener('click', e => {
+            e.preventDefault();
+            gsap.to(window, {
+                scrollTo: e.target.hash
+            })
+        })
+    })
+
+});
+
+
+
+
+
+// $('.menu_tab ul>li').on('click', function (e) {
+//     e.preventDefault();
+//     let idx = $(this).index();
+//     $('.project_wrap>*').eq(idx).addClass('on').siblings().removeClass('on');
+
+// });
+
+const M = document.querySelector('.pr_tab');
+const MT = document.querySelectorAll('.pr_tab a');
+const PJ = document.querySelectorAll('.project_wrap>*');
+
+MT.forEach((it, idx) => {
+    it.addEventListener('click', (e) => {
+        e.preventDefault();
+        MT.forEach(it => it.classList.remove('on'))
+        it.classList.add('on');
+        PJ.forEach(it => it.classList.remove('on'))
+        PJ[idx].classList.add('on');
+    })
+});
+
+let typeSplit = new SplitType('.text_move p', {
+    types: 'lines, words, chars',
+    tagName: 'span'
 })
+
+gsap.from('.word', {
+    y: '100%',
+    opacity: 0,
+    duration: 0.55,
+    ease: 'power3.out',
+    stagger: 0.15,
+    scrollTrigger: {
+        trigger: ".MainIndex",
+        start: "top center",
+    }
+})
+
+gsap.from('.MainProject .inner', {
+    opacity: 0,
+    y: -100,
+    scrub: true,
+    start: "center top",
+});
+
+
+
+
+
+
+
+
+
+
