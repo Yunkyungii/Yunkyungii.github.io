b6f931b4ff38cf1af459534cf0ed6b85ba8ab0bc
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-09-05 18:18:36 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,3 @@
-const panels = gsap.utils.toArray(".MainContent .itm");
-
 
 const BOX =
     gsap.from('.MainIntro .box', {
@@ -28,7 +26,7 @@ gsap.from('.box span', {
 })
 
 
-
+// BOX.repeat(-1);
 
 
 
@@ -93,55 +91,151 @@ MENU_BTN.addEventListener('click', e => {
 
 
 
-const M = document.querySelector('.pr_tab');
-const MT = document.querySelectorAll('.pr_tab a');
-const PJ = document.querySelectorAll('.project_wrap>*');
 
-MT.forEach((it, idx) => {
-    it.addEventListener('click', (e) => {
+
+
+
+
+
+
+
+
+// const panels = gsap.utils.toArray(".MainContent .itm");
+
+// const H = gsap.to(panels, {
+//     xPercent: -100 * (panels.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".MainContent",
+//         pin: true,
+//         start: "top top",
+
+//         scrub: 0, // 숫자에따라 변함이 있음
+//         markers: true,
+//         snap: {
+//             snapTo: 1 / (panels.length - 1),
+//             inertia: false,
+//             duration: { min: 0.1, max: 0.1 }
+//         },
+//         //end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
+//     }
+// });
+
+
+
+
+// const sections = gsap.utils.toArray(".project");
+
+// gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "none",
+//     scrollTrigger: {
+//         trigger: ".MainProject",
+//         pin: true,
+//         scrub: 1,
+//         snap: 1 / (sections.length - 1),
+//         end: "+=7000",
+//         // end: document.querySelector("#parallax__cont").offsetWidth,
+//     }
+// });
+
+//const M = document.querySelector('.pr_tab');
+// const MT = document.querySelectorAll('.pr_tab a');
+// const PJ = document.querySelectorAll('.project_wrap .project');
+
+// MT.forEach((it, idx) => {
+//     it.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         MT.forEach(it =>
+//             it.classList.remove('on'))
+//         it.classList.add('on');
+//         PJ.forEach(it => it.classList.remove('on'))
+//         PJ[idx].classList.add('on');
+//     })
+// });
+
+
+// let links = gsap.utils.toArray('.pr_tab a');
+
+// links.forEach(link => {
+//     let element = document.querySelector(link.getAttribute("href")),
+
+//         linkST = ScrollTrigger.create({
+//             trigger: element,
+//             start: "top top"
+//         });
+
+//     ScrollTrigger.create({
+//         trigger: element,
+//         start: "top center",
+//         end: "bottom center",
+//         onToggle: self => setActive(link)
+//     });
+
+//     link.addEventListener("click", e => {
+//         e.preventDefault();
+//         gsap.to(window, { duration: 1, scrollTo: linkST.start, overwrite: "auto" });
+//     });
+// });
+
+// function setActive(link) {
+//     links.forEach(el => el.classList.remove("active"));
+//     link.classList.add("active");
+// }
+
+
+/* Main navigation */
+let panelsSection = document.querySelector("#MainProject"),
+    panelsContainer = document.querySelector("#panels-container"),
+    tween;
+
+const anchors = document.querySelectorAll(".anchor");
+
+
+anchors.forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
         e.preventDefault();
-        MT.forEach(it => it.classList.remove('on'))
-        it.classList.add('on');
-        PJ.forEach(it => it.classList.remove('on'))
-        PJ[idx].classList.add('on');
-    })
-});
 
-let typeSplit = new SplitType('.text_move p', {
-    types: 'lines, words, chars',
-    tagName: 'span'
-})
+        anchors.forEach(it => it.classList.remove('on'))
+        e.target.classList.add('on');
 
-gsap.from('.word', {
-    y: '100%',
-    opacity: 0,
-    duration: 0.55,
-    ease: 'power3.out',
-    stagger: 0.15,
-    scrollTrigger: {
-        trigger: ".MainIndex",
-        start: "top center",
-    }
-})
+        let targetElem = document.querySelector(e.target.getAttribute("href")),
+            y = targetElem;
+        if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
+            let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+                totalMovement = (panels.length - 1) * targetElem.offsetWidth;
+            y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
+        }
+        gsap.to(window, {
+            scrollTo: {
+                y: y,
+                autoKill: false
+            },
+            duration: 1,
+        });
 
-gsap.from('.MainProject .inner', {
-    opacity: 0,
-    y: -100,
-    scrub: true,
-    start: "center top",
+    });
 });
 
 
-gsap.from('.MainContact .inner', {
-    y: 300,
-    duration: 2,
+
+
+/* Panels */
+const panels = gsap.utils.toArray("#panels-container .project");
+tween = gsap.to(panels, {
+    xPercent: -100 * (panels.length - 1),
+    ease: "none",
     scrollTrigger: {
-        trigger: ".MainIndex",
-        start: "top center",
+        trigger: "#MainProject",
+        pin: true,
+        start: "top top",
+        scrub: 1,
+        snap: {
+            snapTo: 1 / (panels.length - 1),
+            inertia: false,
+            duration: { min: 0.1, max: 0.1 }
+        },
+        end: () => "+=" + (panelsContainer.offsetWidth - innerWidth)
     }
-})
-
-
-
-
+});
 
